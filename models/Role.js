6b25789f952621cc7bd286b6123566855821aa9e
Vitaliy Chalin <vitaly.chalin@gmail.com>
2020-09-16 14:19:30 +0500

    Remove model Role

--- models/Role.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const schema = new Schema({
-	roles: ['all', 'admin', 'curator', 'student', 'teacher'],
-});
-
-module.exports = model('Role', schema);
