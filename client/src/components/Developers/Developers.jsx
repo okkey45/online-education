a6d23efa2ef4a032b4a4c20d322b97a2b6d448d6
Evgeniya <ved-evgeniya@yandex.ru>
2020-09-17 21:20:18 +0300

    Изменила код страницы Home и страницы Developers

--- client/src/components/Developers/Developers.jsx
+++ client/src/components/Developers/Developers.jsx
@@ -11,75 +11,22 @@ import { AuthContext } from '../../context/AuthContext';
 export const Developers = () => {
     const { loading, request } = useHttp();
     const [users, setUsers] = useState([]);
-    const [usersGroups, setUsersGroups] = useState([]);
-    const [groups, setGroups] = useState([]);
-    const { token } = useContext(AuthContext);
+	const { token } = useContext(AuthContext);
+
 
     const getUsers = useCallback(async () => {
 		try {
 			const data = await request('/api/user', 'GET', null, {
 				Authorization: `Bearer ${token}`,
 			});
-            setUsers(data);
+			setUsers(data);
 		} catch (e) {}
 	}, [token, request]);
 
 	useEffect(() => {
 		getUsers();
 	}, [getUsers]);
-    const getUsersGroups = useCallback(async () => {
-		try {
-			const data = await request(`/api/user_groups`, 'GET', null, {
-				Authorization: `Bearer ${token}`,
-			});
-            setUsersGroups(data);
-            
-		} catch (e) {}
-    }, [token, request]);
-    
-    useEffect(() => {
-		getUsersGroups();
-    }, [getUsersGroups]);
-
-    const getGroups = useCallback(async () => {
-		try {
-			const data = await request('/api/group', 'GET', null, {
-				Authorization: `Bearer ${token}`,
-			});
-			setGroups(data);
-		} catch (e) {}
-	}, [token, request]);
-
-	useEffect(() => {
-		getGroups();
-	}, [getGroups]);
-
-	const getGroupName = (groupId) => {
-		let group = [];
-		groups.forEach((el) => {
-			if (el._id === groupId) {
-				group['id'] = el._id;
-				group['name'] = el.name;
-				group['description'] = el.description;
-			}
-		});
-        return group;
-	};
-    const getUserGroups = (userId) => {
-		const user_groups = usersGroups.filter((group) => group.user_id === userId);
-		const userGroupsArr = [];
-
-		if (user_groups[0]) {
-			const { group_ids } = user_groups[0];
-			group_ids.forEach((id) => {
-				const group = getGroupName(id);
-				userGroupsArr.push(group);
-			});
-		}
-		return userGroupsArr;
-	};
-
-    
+	
 	return (
 		<>
             <div className="table-responsive mb-3">
@@ -92,24 +39,17 @@ export const Developers = () => {
 				</thead>
 				<tbody>
 					{users.map((user, i) => {
-						const user_groups = getUserGroups(user._id);
+						const userRoles = user.roles.filter(rol => rol === 'admin')
+							if (String(userRoles) === 'admin') {
 							   return ( 
-                           			 <>
-										{user_groups.length > 0 &&
-										user_groups.map((el, i) => {
-											if(el.id === '5f53768c623f050aa4a2f3aa') {
-											return ( 
-												<tr key={user._id}>
-														<td>{user.name}</td>
-														<td>{user.email}</td>
-												</tr>
-												)
-											}
-										})
-									}
-								</>
-							)
-						})
+									<tr key={i}>
+										<td>{user.name}</td>
+										<td>{user.email}</td>	
+									</tr>
+							  		)
+								}	
+							}
+						)		
 					}
 				</tbody>
 			</table>
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,13 +4,13 @@ export const Home = () => {
 	return (
 		<>
 			<div className="widget__wrapper has-shadow">
-				<div className="widget__body">Добро пожаловать в MyNewLife club!</div>
-			</div>
-			<div className="home">
-				<div className="widget__wrapper has-shadow home-training">Тренинг по психологии</div>
-				<div className="widget__wrapper has-shadow home-training">Тренинг по психологии</div>
-				<div className="widget__wrapper has-shadow home-training">Тренинг по психологии</div>
-				<div className="widget__wrapper has-shadow home-training">Тренинг по психологии</div>
+				<div className="widget__header">Добро пожаловать в MyNewLife club!</div>
+				<div className="widget__body home">
+					<div className="card home-training">Тренинг по психологии</div>
+					<div className="card home-training">Тренинг по психологии</div>
+					<div className="card home-training">Тренинг по психологии</div>
+					<div className="card home-training">Тренинг по психологии</div>
+				</div>
 			</div>
 		</>
 	);
