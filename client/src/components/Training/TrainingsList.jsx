6ffa68e08848d53bf3a5d0b598ae83698c455d85
DimaLom <lom797@gmail.com>
2020-09-21 23:17:08 +0700

    getTrainingsByUserId

--- client/src/components/Training/TrainingsList.jsx
+++ client/src/components/Training/TrainingsList.jsx
@@ -1,34 +1,32 @@
-import React, { useState, useEffect, useCallback, useContext } from 'react';
-import { useHttp } from '../../hooks/http.hook';
-import { AuthContext } from '../../context/AuthContext';
-import { Loader } from '../Loader/Loader';
-import { TrainingsListItem } from './TrainingsListItem';
+import React, { useState, useEffect, useCallback, useContext } from "react";
+import { useHttp } from "../../hooks/http.hook";
+import { AuthContext } from "../../context/AuthContext";
+import { Loader } from "../Loader/Loader";
+import { TrainingsListItem } from "./TrainingsListItem";
 
 export const TrainingsList = () => {
-	const { loading, request } = useHttp();
-	const [trainings, setTrainings] = useState([]);
-	const { token } = useContext(AuthContext);
+  const { loading, request } = useHttp();
+  const [trainings, setTrainings] = useState([]);
+  const { token } = useContext(AuthContext);
 
-	const getTrainings = useCallback(async () => {
-		try {
-			const data = await request('/api/training', 'GET', null, {
-				Authorization: `Bearer ${token}`,
-			});
-			setTrainings(data);
-		} catch (e) {}
-	}, [token, request]);
+  const getTrainings = useCallback(async () => {
+    try {
+      const data = await request("/api/training", "GET", null, {
+        Authorization: `Bearer ${token}`,
+      });
+      setTrainings(data);
+    } catch (e) {}
+  }, [token, request]);
 
-	useEffect(() => {
-		getTrainings();
-	}, [getTrainings]);
+  useEffect(() => {
+    getTrainings();
+  }, [getTrainings]);
 
-	if (loading) {
-		return <Loader />;
-	}
-
-	return (
-		<div className="widget__body">
-			<TrainingsListItem trainings={trainings} />
-		</div>
-	);
+  return loading ? (
+    <Loader />
+  ) : (
+    <div className="widget__body">
+      <TrainingsListItem trainings={trainings} />
+    </div>
+  );
 };
