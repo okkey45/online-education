1ccb4f0c5320ed6d33bef2bad78879db7b4be016
Evgeniya <ved-evgeniya@yandex.ru>
2020-09-25 10:59:21 +0300

    Добавила страницу ответ студента

--- client/src/components/Subject/SubjectDetail.jsx
+++ client/src/components/Subject/SubjectDetail.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState, useCallback, useContext, useEffect } from 'react';
+import { useHttp } from '../../hooks/http.hook';
+import { AuthContext } from '../../context/AuthContext';
+import { AnswersStudent } from '../Answers/AnswersStudent.jsx';
 
 export const SubjectDetail = ({ subject }) => {
-	return (
+	const { loading, request } = useHttp();
+	const [users, setUsers] = useState([]);
+    const { token } = useContext(AuthContext);
+
+
+	const getUsers = useCallback(async () => {
+		try {
+			const data = await request('/api/user', 'GET', null, {
+				Authorization: `Bearer ${token}`,
+			});
+			setUsers(data);
+		} catch (e) {}
+	}, [token, request]);
+
+	useEffect(() => {
+		getUsers();
+    }, [getUsers]);
+
+	return (	
 		<div className="widget__wrapper has-shadow">
 			<div className="widget__header">
 				<h4 className="widget__title">{subject.title}</h4>
 			</div>
-			<div className="widget__body">{subject.context}</div>
+			<div className="widget__body">
+				<div className="mb-3">{subject.context}</div>
+				<div>{ 
+						<AnswersStudent />
+					}
+				</div>
+			</div>
 		</div>
 	);
 };
