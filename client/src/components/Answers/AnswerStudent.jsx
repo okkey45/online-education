71ff1c9b20e669d62c0803e662dcf35971e9b083
Evgeniya <ved-evgeniya@yandex.ru>
2020-10-02 11:18:43 +0300

    Убрала console

--- client/src/components/Answers/AnswerStudent.jsx
+++ client/src/components/Answers/AnswerStudent.jsx
@@ -2,12 +2,10 @@ import React, { useState, useCallback, useContext, useEffect } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { useHttp } from '../../hooks/http.hook';
 import { useParams } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
 
 import { Form, Button, Col } from 'react-bootstrap';
 export const AnswerStudent = () => {
     const subjectId = useParams().id;
-    const history = useHistory();
     const { request, loading } = useHttp();
     const { token, userRoles, userId }  = useContext(AuthContext);
     const [ answerTeacher, setAnswerTeacher ]  = useState([]);
@@ -43,7 +41,6 @@ export const AnswerStudent = () => {
                 { Authorization: `Bearer ${token}` },
             );
             setAnswerStud(data)
-            // history.push(`/subject/${subjectId}`);
 		} catch (e) {}
     };
 //     const sendAnswerTeacher = async (event) => {
@@ -54,8 +51,7 @@ export const AnswerStudent = () => {
 //                 { ...formTeach },
 //                 { Authorization: `Bearer ${token}` },
 //             );
-//             setAnswerTeacher(data);
-//             //console.log(data);  
+//             setAnswerTeacher(data); 
 // console.log(data);
 // 		} catch (e) {}
 //     };
@@ -65,7 +61,6 @@ export const AnswerStudent = () => {
 				Authorization: `Bearer ${token}`,
             });
             setAnswerStud(data)
-            console.log(data);
 		} catch (e) {}
     }, [token, request, subjectId]);
         
@@ -73,100 +68,96 @@ export const AnswerStudent = () => {
 		getAnswers();
     }, [getAnswers, subjectId]);
     const answerMap = () => {
-    if (answerStud.length > 0) {
-        const author = answerStud.map(aut => aut.author_id)
-        const author2 = author.filter(a => a == userId)
-        const author3 = String(author2[0])
-    return author3
+        if (answerStud.length > 0) {
+            const author = answerStud.map(aut => aut.author_id)
+            const author2 = author.filter(a => a == userId)
+            const author3 = String(author2[0])
+        return author3
     }}
     const answerFilter = () => {
         if (answerStud.length > 0) { 
             const ans = answerStud.filter(a => a.author_id == userId)
             return ans
     }}
-    console.log(answerFilter(answerStud));
 
        return ( 
                 <>
-                { (String(userRolStudent) === 'student') && 
-                    <div>
-                        { 
-                        (answerStud.length < 1 || answerMap(answerStud) !== userId)  && 
-                            <Form className="form__createTraining mb-3" onSubmit={sendAnswer}>
-                            <Form.Group controlId="pleForm.ControlTextarea1" className="mb-3">
-                                <Form.Label>Ответ студента:</Form.Label>
-                                <Form.Control
-                                as="textarea"
-                                rows="3" 
-                                className="textarea-answer"
-                                name="stud_response"
-                                value={form.stud_response}
-                                onChange={changeHandler}
-                                />
-                            </Form.Group>
-                            <Button
-                            type="submit"
-                            // onClick={sendAnswer}
-                            disabled={loading}
-                         
-                            >
-                                Отправить
-                            </Button>
-                        </Form >                       
-                        }
-                        {   
-                            (answerStud.length > 0 &&  answerMap(answerStud) === userId) &&
-                            answerFilter(answerStud).map((an, i) => {
-                               
-                            return (
-                                <div key={i}>
-                                    <p className="mb-3">Ответ:</p>
-                                    <p>{an.stud_response}</p>
-                                </div>  
-                                )
-                            })  
-                        }
-                    </div>
-                }
-                {
-                 // (String(userRolTeacher) === 'teacher') && 
-                //   <div>
-                //       {
-                //           answerStud.length > 0  &&        
-                //           answerStud.map((an, i) => {
-        
-                //           return (
-                //               <div key={i}>
-                //                   <p>Ответ студента:</p>
-                //                   <p className="mt-3">{an.stud_response}</p>
-                //               </div> 
-                //               )
-                //           })
-                //       }
-                //       {                         
-                //     <Form className="form__createTraining mb-3">
-                //          <Form.Group controlId="pleForm.ControlTextarea1" className="mb-3">
-                //              <Form.Label>Комментарий учителя:</Form.Label>
-                //              <Form.Control
-                //              as="textarea"
-                //              rows="3" 
-                //              className="textarea-answer"
-                //              name="prof_response"
-                //              value={formTeach.prof_response}
-                //              onChange={changeHandlerTeach}
-                //              />
-                //          </Form.Group>
-                //          <Button
-                //          type="submit"
-                //          onClick={sendAnswerTeacher}
-                //          disabled={loading}
-                //          >
-                //              Отправить
-                //          </Button>
-                //      </Form>                    
-                //       }
-                //   </div>
-                }  
+                    { (String(userRolStudent) === 'student') && 
+                        <div>
+                            { 
+                            (answerStud.length < 1 || answerMap(answerStud) !== userId)  && 
+                                <Form className="form__createTraining mb-3" onSubmit={sendAnswer}>
+                                <Form.Group controlId="pleForm.ControlTextarea1" className="mb-3">
+                                    <Form.Label>Ответ студента:</Form.Label>
+                                    <Form.Control
+                                    as="textarea"
+                                    rows="3" 
+                                    className="textarea-answer"
+                                    name="stud_response"
+                                    value={form.stud_response}
+                                    onChange={changeHandler}
+                                    />
+                                </Form.Group>
+                                <Button
+                                type="submit"
+                                disabled={loading}                            
+                                >
+                                    Отправить
+                                </Button>
+                            </Form >                       
+                            }
+                            {   
+                                (answerStud.length > 0 &&  answerMap(answerStud) === userId) &&
+                                answerFilter(answerStud).map((an, i) => {                               
+                                return (
+                                    <div key={i}>
+                                        <p className="mb-3">Ответ:</p>
+                                        <p>{an.stud_response}</p>
+                                    </div>  
+                                    )
+                                })  
+                            }
+                        </div>
+                    }
+                    {
+                    // (String(userRolTeacher) === 'teacher') && 
+                    //   <div>
+                    //       {
+                    //           answerStud.length > 0  &&        
+                    //           answerStud.map((an, i) => {
+            
+                    //           return (
+                    //               <div key={i}>
+                    //                   <p>Ответ студента:</p>
+                    //                   <p className="mt-3">{an.stud_response}</p>
+                    //               </div> 
+                    //               )
+                    //           })
+                    //       }
+                    //       {                         
+                    //     <Form className="form__createTraining mb-3">
+                    //          <Form.Group controlId="pleForm.ControlTextarea1" className="mb-3">
+                    //              <Form.Label>Комментарий учителя:</Form.Label>
+                    //              <Form.Control
+                    //              as="textarea"
+                    //              rows="3" 
+                    //              className="textarea-answer"
+                    //              name="prof_response"
+                    //              value={formTeach.prof_response}
+                    //              onChange={changeHandlerTeach}
+                    //              />
+                    //          </Form.Group>
+                    //          <Button
+                    //          type="submit"
+                    //          onClick={sendAnswerTeacher}
+                    //          disabled={loading}
+                    //          >
+                    //              Отправить
+                    //          </Button>
+                    //      </Form>                    
+                    //       }
+                    //   </div>
+                    }  
                 </>
             );
            
